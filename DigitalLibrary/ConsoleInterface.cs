95813ea318c7adb2ef70535d0a5bcac5483de156
Anton <anton.tabashniuk@email.com>
2026-05-17 01:09:26 +0300

    feat: setup main Console App entry point and Dependency Injection - create main Presentation project (entry point) including Program.cs and ConsoleInterface. - install Microsoft.Extensions.DependencyInjection package for DI container setup. - configure Dependency Injection pipeline by registering IBookRepository, LibraryService, and ConsoleInterface as Singletons.

---
+++ DigitalLibrary/ConsoleInterface.cs
@@ -0,0 +1,12 @@
+﻿using DigitalLibrary.BusinessLogic;
+namespace DigitalLibrary
+{
+    public class ConsoleInterface
+    {
+        private readonly LibraryService _libraryService;
+        public ConsoleInterface(LibraryService libraryService)
+        {
+            _libraryService = libraryService ?? throw new ArgumentNullException(nameof(libraryService));
+        }
+    }
+}
--- DigitalLibrary/Program.cs
+++ DigitalLibrary/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DigitalLibrary;
+using DigitalLibrary.BusinessLogic;
+using DigitalLibrary.DataAccess.BookRepository;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IBookRepository>(new JsonBookRepository("library_data.json"))
+    .AddSingleton<LibraryService>()
+    .AddSingleton<ConsoleInterface>()
+    .BuildServiceProvider();
+
+using var scope = serviceProvider.CreateScope(); 
+
+var consoleInterface = scope.ServiceProvider.GetRequiredService<ConsoleInterface>();
+
+await consoleInterface.StartProgram();
