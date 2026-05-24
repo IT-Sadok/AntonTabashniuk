bf99711a32351fb7a79a67031823b3be334ac2d2
Anton <anton.tabashniuk@gmail.com>
2026-05-24 20:17:37 +0300

    test: implement concurrency stress tests for CRUD operations - Implement StressRunner to orchestrate high-load concurrent access (e.g., 100 parallel workers). - Create ConcurrentWorker to simulate random user behavior (Add, Delete, Borrow, Return, Read) with randomized delays. - Add StressStatistics utilizing Interlocked operations for thread-safe metrics tracking. - Introduce RandomBookFactory for generating mock book entities during testing.

--- DigitalLibrary.ConcurrentTests/Program.cs
+++ DigitalLibrary.ConcurrentTests/Program.cs
@@ -1,8 +1,10 @@
 ﻿using DigitalLibrary.BusinessLogic;
 using DigitalLibrary.ConcurrentTests;
+using DigitalLibrary.DataAccess.BookRepository;
 using Microsoft.Extensions.DependencyInjection;
 
 var serviceProvider = new ServiceCollection()
+    .AddSingleton<IBookRepository>(new JsonBookRepository("testLoad.json"))
     .AddSingleton<ILibraryService, LibraryService>()
     .AddSingleton<StressRunner>()
     .BuildServiceProvider();
--- DigitalLibrary.ConcurrentTests/StressRunner.cs
+++ DigitalLibrary.ConcurrentTests/StressRunner.cs
@@ -12,7 +12,47 @@ public class StressRunner
 
     public async Task RunAsync()
     {
+        Console.WriteLine("Starting stress test...");
+
+        CancellationTokenSource cts = new(TimeSpan.FromSeconds(10));
+
+        StressStatistics statistics = new StressStatistics();
         
+        List<Task> tasks = new();
+
+        int workersCount = 100;
+
+        for (int i = 0; i < workersCount; i++)
+        {
+            ConcurrentWorker worker = new(_libraryService, statistics);
+
+            tasks.Add(worker.RunAsync(cts.Token));
+        }
+
+        try
+        {
+            await Task.WhenAll(tasks);
+        }
+        catch
+        {
+        }
+
+        Console.WriteLine("\nStress test completed.");
+        PrintStatistics(statistics);
+        Console.ReadKey();
     }
-    
+
+    private void PrintStatistics(StressStatistics statistics)
+    {
+        Console.WriteLine("=== RESULTS ===");
+
+        Console.WriteLine($"Added: {statistics.AddedBooks}");
+        Console.WriteLine($"Removed: {statistics.RemovedBooks}");
+        Console.WriteLine($"Borrowed: {statistics.BorrowedBooks}");
+        Console.WriteLine($"Returned: {statistics.ReturnedBooks}");
+        Console.WriteLine($"Read: {statistics.ReadOperations}");
+        Console.WriteLine($"Exceptions: {statistics.Exceptions}");
+
+    }
 }
+    
