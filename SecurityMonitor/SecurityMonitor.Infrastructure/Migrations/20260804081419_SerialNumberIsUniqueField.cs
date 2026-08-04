c52262e27f27991ca0a8e7a1ddd87009d4e7abe7
Anton <anton.tabashniuk@gmail.com>
2026-08-04 11:27:29 +0300

    fix: added missing migration for SerialNumber.IsUnique()

---
+++ SecurityMonitor/SecurityMonitor.Infrastructure/Migrations/20260804081419_SerialNumberIsUniqueField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecurityMonitor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SerialNumberIsUniqueField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_devices_serial_number",
+                table: "devices",
+                column: "serial_number",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_devices_serial_number",
+                table: "devices");
+        }
+    }
+}
